f54dc96edb696527a24ee5675bb553971d3ff700
Bryan Mehr <bryanmehr@gmail.com>
2025-06-16 16:20:12 -0700

    changes

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
+renderer.useLegacyLights = false; // Use physically correct lighting
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
