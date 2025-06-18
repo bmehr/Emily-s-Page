f491748e0e250186016e851babc0a9beeaa3313e
Bryan Mehr <bryanmehr@gmail.com>
2025-06-18 10:08:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
   // ... other imports from CDN
 </script>
 
-import * as THREE from 'three';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
