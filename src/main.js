f4b6ed4762cf989649c7522188766bf9d3923ca4
Bryan Mehr <bryanmehr@gmail.com>
2025-06-18 13:46:07 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// import * as THREE from 'three';
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import { initScene } from './scene.js';
 import { setupControls } from './controls.js';
 import { setupInteractions } from './interactions.js';
